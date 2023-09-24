c04c8f14f3ce623ab969e64fe254cad13c860b28
Pylogmon <pylogmon@outlook.com>
2023-09-24 22:17:01 +0800

    feat: YouDao WordList

--- src/lib.rs
+++ src/lib.rs
@@ -5,20 +5,33 @@ use std::error::Error;
 #[no_mangle]
 pub fn collection(
     source: &str,
-    to: &str,
+    _to: &str,
     needs: HashMap<String, String>,
 ) -> Result<Value, Box<dyn Error>> {
     let client = reqwest::blocking::ClientBuilder::new().build()?;
-    let port = match needs.get("port") {
-        Some(port) => port.to_string(),
-        None => "8765".to_string(),
+    let cookie = match needs.get("cookie") {
+        Some(cookie) => cookie.to_string(),
+        None => return Err("cookie is required".into()),
     };
 
-    let url = format!("http://localhost:{port}");
-
-    // Impl...
-
-    Ok(Value::Bool(true))
+    let res: Value = client
+        .get("https://dict.youdao.com/wordbook/webapi/v2/ajax/add")
+        .header("Cookie", cookie)
+        .query(&[("word", source), ("lan", "en")])
+        .send()?
+        .json()?;
+    if let Some(json) = res.as_object() {
+        if let Some(code) = json.get("code") {
+            if code.as_i64().unwrap() == 0 {
+                return Ok(Value::Bool(true));
+            } else {
+                if let Some(msg) = json.get("msg") {
+                    return Ok(Value::String(msg.as_str().unwrap().to_string()));
+                }
+            }
+        }
+    }
+    Ok(Value::Bool(false))
 }
 
 #[cfg(test)]
@@ -27,8 +40,8 @@ mod tests {
     #[test]
     fn try_request() {
         let mut needs = HashMap::new();
-        needs.insert("port".to_string(), "8765".to_string());
-        let result = collection("Hello", "你好", needs).unwrap();
+        needs.insert("cookie".to_string(), "".to_string());
+        let result = collection("Hello", "en", needs).unwrap();
         println!("{result}");
     }
 }
